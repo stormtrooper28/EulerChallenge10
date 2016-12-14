d2b8792c5533c00c9856d314c67a330906c18377
storm null <stormtrooper28@live.com>
2016-12-13 21:37:30 -0500

    Fixed ArrayOutOfBounds Error (no exception thrown) and added question to top.

--- EulerChallenge7/EulerChallenge7.cpp
+++ EulerChallenge7/EulerChallenge7.cpp
@@ -1,11 +1,39 @@
 // EulerChallenge7.cpp : Defines the entry point for the console application.
-//
+/*
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+
+*/
 
 #include "stdafx.h"
+#include <iostream>
+
+using namespace std;
 
 
-int main()
-{
+int main() {
+	short index = 1;
+	unsigned long primes[10001];
+	primes[0] = 2;
+	unsigned long numberToCheck = 3;
+	while (index < 10001) {
+		bool divisible = false;
+		for (int c = 0; c < index; c++) {
+			if (numberToCheck % primes[c] == 0) {
+				divisible = true;
+				break;
+			}
+		}
+		if (!divisible)
+			primes[index++] = numberToCheck;
+		
+		numberToCheck++;
+	}
+	cout << "The 10001th prime is " << primes[10000];
+
+	cin >> index;
     return 0;
 }
 
